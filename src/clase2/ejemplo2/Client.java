c475b13087e6194657e01eca400232a11d2a75ca
xvarria <mchavarria@gmail.com>
2017-10-02 19:00:50 -0600

    Se agrega nueva implementacion utilizando el servicio especifico creado por el wsimport

--- src/clase2/ejemplo2/Client.java
+++ src/clase2/ejemplo2/Client.java
@@ -6,10 +6,20 @@ public class Client {
 
 	public static void main(String[] args) {
 
+		//Usando Interfaz Service - modo genérico
 		Service service = new TimeServerImplService(); //Se instancia el serivico creado automaticamente
 		TimeServer eif = service.getPort(TimeServer.class);// Se instancia el port con la interface Server creado automaticamente
+		
 		System.out.println(eif.getTimeServiceAsLong());
 		System.out.println(eif.getTimeServiceAsString());
-	}
-
+		
+		
+		//Usando implementación específica
+		TimeServerImplService service2 = new TimeServerImplService();
+		TimeServer eif2 = service2.getTimeServerImplPort();
+		
+		System.out.println(eif2.getTimeServiceAsLong());
+		System.out.println(eif2.getTimeServiceAsString());
+	
+	}		
 }
